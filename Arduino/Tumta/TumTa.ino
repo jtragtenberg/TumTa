da2432a888d35806765157f35ebbe7ac35558c92
João Tragtenberg <tragtenberg@gmail.com>
2025-05-21 21:17:34 -0300

    v0_3_1

--- Arduino/Tumta/TumTa.ino
+++ Arduino/Tumta/TumTa.ino
@@ -1,97 +1,162 @@
-byte message[6];
-boolean isRawData = false;
+// Tum
+#define TUM_PIN A0
+#define TUM_CHANNEL 0
+// Ta
+#define TA_PIN A1
+#define TA_CHANNEL 1
 
-int threshold; //vai ser definido por um potenciometro
+//Mode Switch
+#define MODE_SWITCH_PIN 12
+
+//Threshold Pot
+#define THRESHOLD_PIN A7
+
+//Standby Switch
+#define STANDBY_SWITCH_PIN 2
+
+//Led
+#define LED_PIN 6
+
+
+bool isDebugging = false;
+byte debugMessage[15];
+byte liveMessage[7];
+
+int threshold = 20;
 int debounceTime = 150;
 
-long previousMillis = 0;
-long interval = 5;
-
-void setup() 
+void setup()
 {
   Serial.begin(38400);
-  pinMode(A0, INPUT);
-  pinMode(A7, INPUT);
+  pinMode(TUM_PIN, INPUT);
+  pinMode(TA_PIN, INPUT);
+  pinMode(MODE_SWITCH_PIN, INPUT);
+  pinMode(THRESHOLD_PIN, INPUT);
+  pinMode(STANDBY_SWITCH_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
   analogReference(EXTERNAL);
+
+  digitalWrite(LED_PIN, HIGH);
 }
 
-void loop() 
-{
-  unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis > interval)
-  {
-    previousMillis = currentMillis;   
+boolean standby = false;
 
-    int channel;
-    threshold = analogRead(A7)/4;
-    // Read ANALOG A0
-    if(!isRawData)
+void loop()
+{
+  standby = !digitalRead(STANDBY_SWITCH_PIN);
+  if (standby) {
+    digitalWrite(LED_PIN, LOW);
+  }
+  else {
+    digitalWrite(LED_PIN, HIGH);
+    //isDebugging = digitalRead(MODE_SWITCH_PIN);
+
+
+    // Tum
+    int tumPressure = getAvarage(TUM_CHANNEL, analogRead(TUM_PIN));
+    int tumDerivative = getDerivative(TUM_CHANNEL, tumPressure);
+    int tumIntensity = getIntensity(TUM_CHANNEL, tumDerivative);
+    // Ta
+    int taPressure = getAvarage(TA_CHANNEL, analogRead(TA_PIN));
+    int taDerivative = getDerivative(TA_CHANNEL, taPressure);
+    int taIntensity = getIntensity(TA_CHANNEL, taDerivative);
+    // Message
+    if(isDebugging)
     {
-      int newValue = sendAvarageValue(0); //channel 0: average pressure
-      int newDerivative = sendDerivativeValue(1, 0, newValue); //channel 1: derivative
-      sendIntensityValue(2, newDerivative); //channel 2: intensity triggers
+      setDebugMessage(tumPressure, taPressure, tumDerivative, taDerivative, tumIntensity, taIntensity); 
+      Serial.write(debugMessage, sizeof(debugMessage));
     }
     else
     {
-      setMessage(0, analogRead(A0));
+      setLiveMessage(tumIntensity, taIntensity); 
+      Serial.write(liveMessage, sizeof(liveMessage));
     }
+    delay(5);
   }
-
 }
 
-void setMessage(int channel, int value)
+// MESSAGE
+void setDebugMessage(int tumPressure, int taPressure, int tumDerivative, int taDerivative, int tumIntensity, int taIntensity)
 {
   // Start Header
-  message[0] = 0xFF;
-  message[1] = 0xFF;
-  // Channel
-  message[2] = byte(channel);
-  // Value
-  message[3] = lowByte(value);
-  message[4] = highByte(value);
+  debugMessage[0] = 0xFF;
+  debugMessage[1] = 0xFF;
+  // Pressure Tum
+  debugMessage[2] = lowByte(tumPressure);
+  debugMessage[3] = highByte(tumPressure);
+  // Pressure Ta
+  debugMessage[4] = lowByte(taPressure);
+  debugMessage[5] = highByte(taPressure);
+  // Derivative Tum
+  debugMessage[6] = lowByte(tumDerivative);
+  debugMessage[7] = highByte(tumDerivative);
+  // Derivative Ta
+  debugMessage[8] = lowByte(taDerivative);
+  debugMessage[9] = highByte(taDerivative);
+  // Derivative Tum
+  debugMessage[10] = lowByte(tumIntensity);
+  debugMessage[11] = highByte(tumIntensity);
+  // Derivative Ta
+  debugMessage[12] = lowByte(taIntensity);
+  debugMessage[13] = highByte(taIntensity);
   // Checksum
-  message[5] = (message[2] + message[3] + message[4]);
+  debugMessage[14] = 0;
+  for(int i = 2; i < 14; i++)
+  {
+    debugMessage[14] += debugMessage[i];
+  }
+}
+
+void setLiveMessage(int tumIntensity, int taIntensity)
+{
+  // Start Header
+  liveMessage[0] = 0xFE;
+  liveMessage[1] = 0xFE;
+  // Intensity Tum
+  liveMessage[2] = lowByte(tumIntensity);
+  liveMessage[3] = highByte(tumIntensity);
+  // Intensity Ta
+  liveMessage[4] = lowByte(taIntensity);
+  liveMessage[5] = highByte(taIntensity);
+  // Checksum
+  liveMessage[6] = 0;
+  for(int i = 2; i < 6; i++)
+  {
+    liveMessage[6] += liveMessage[i];
+  }
 }
 
 // Avarage
 
-#define numReadings 3
-int values[1][numReadings];
-int total[1];
+#define numReadings 5
+int values[2][numReadings];
+int total[2];
 int index = 0;
 int avrg = 0;
 
-int avarage(int channel,int value)
+int getAvarage(int channel, int value)
 {
-  total[channel] = total[channel] - values[channel][index];         
+  total[channel] = total[channel] - values[channel][index];
   values[channel][index] = value;
 
   total[channel] = total[channel] + values[channel][index];
-  index = index + 1;                    
+  index = index + 1;
 
-  if (index >= numReadings)              
+  if (index >= numReadings)
   {
-    index = 0;                           
+    index = 0;
   }
-  avrg = (int) total[channel] / numReadings;  
-  return avrg;  
-}
-
-int sendAvarageValue(int channel)
-{
-  int valueAvrg = avarage(channel, analogRead(A0));
-  setMessage(0, valueAvrg);
-  Serial.write(message, 6);
-  return valueAvrg;
+  avrg = (int) total[channel] / numReadings;
+  return avrg;
 }
 
 // Derivative
 
-int valuesForDerivative[1][5];
+int valuesForDerivative[2][5];
 float derivativeValues[10] = {
-  0,0,0,0,0,0,0,0,0,0};
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-int derivative(int channel, int value)
+int getDerivative(int channel, int value)
 {
   // Derivada
   valuesForDerivative[channel][4] = valuesForDerivative[channel][3];
@@ -105,57 +170,25 @@ int derivative(int channel, int value)
   return derivativeValues[0];
 }
 
-int sendDerivativeValue(int channel, int fromChannel, int newValue)
-{
-  int derivativeValue = derivative(fromChannel, newValue);
-  setMessage(channel, derivativeValue + 512);
-  Serial.write(message, 6);
-  return derivativeValue;
-}
-
-
 // Intensity
 
+bool isAboveThreshold = false;
 int intensity = 0;
+int antDerivative;
+int derivativeMaxValue;
 
-int debouncing;
-long lastDebounceTime;
-int aindaNao;
-int maximo;
-
-void sendIntensityValue(int channel, int newDerivative)
+int getIntensity(int channel, int newDerivative)
 {
-
-  if (!debouncing){                    //se nao estiver no meio de uma outro pisada
-    if(newDerivative > threshold){       //define a pisada quando a derivada passa de um threshold
-      lastDebounceTime = millis();     //guarda na memoria o instante que aconteceu a pisada no Tum
-      debouncing = true;                                      //diz que ainda esta debouncing      
-    }
+  intensity = 0;
+  derivativeMaxValue = (derivativeMaxValue < newDerivative) ? newDerivative : derivativeMaxValue;
+  if (antDerivative > newDerivative && newDerivative > threshold)
+  {
+    intensity = derivativeMaxValue;
+    derivativeMaxValue = 0;
   }
+  antDerivative = newDerivative;
 
-  //Agoritmo para controlar o estado de debounce
-  if ((millis() - lastDebounceTime) > debounceTime) {       //Controla quando esta debouncing. Se ja passou o tempo de debounce
-    debouncing = false;                                     //apos a ultima pisada diz que ja saiu do debounce
-    intensity = 0;                             //avisa que parou o tempo de debounce do Ta
-    aindaNao = true;                             //reinicia o aindaNao para permitir que a proxima pisada venha e que nao role outro noteOff...
-    maximo = 0;                                  //zera o valor do maximo para a proxima poder chegar...    
-  } 
-
-
-  //Algoritmo para deteccao da pisada
-  if (debouncing) {                               //enquanto estiver no tempo do debounce
-    if (aindaNao){                                //se ainda nao tiver descoberto o maximo da derivada (a intensidade da pisada)
-      if (newDerivative > maximo){               //se o grafico da derivada esta crescendo
-        maximo = newDerivative;                  //continue colocando no valor da derivada na variavel maximo
-      }
-      else {                                         //se o grafico da derivada parar de crescer
-        intensity = maximo - threshold;                  //pega o ultimo valor do maximo e guarda o valor na variavel intensidade
-        aindaNao = false;                         //a pisada ja rolou
-      }
-    }
-  }
-
-  setMessage(channel, intensity);
-  Serial.write(message, 6);
+  return intensity;
 }
+
 
